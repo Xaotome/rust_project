e52f9273d4260bf5bed829cb9386b30f44be364a
Thomas Pecriaux <thomas.pecriaux@gmail.com>
2026-04-02 10:57:19 +0200

    Ex2 - Création calculatrice + Gestion des erreurs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,96 @@
 fn main() {
-    ex1();
+    //ex1();
+    ex2();
+}
+
+fn ex2() {
+    println!("Bienvenue sur votre calculatrice personnelle !");
+
+    let mut option = 1;
+
+    while option != 0 {
+        println!("Veuillez choisir une option :");
+        println!("1. Addition");
+        println!("2. Soustraction");
+        println!("3. Multiplication");
+        println!("4. Division");
+        println!("0. Quitter");
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).expect("Erreur de lecture");
+        option = input.trim().parse().expect("Veuillez entrer un nombre valide");
+
+        match option {
+            1 => 
+            {
+                println!("Entrez le premier nombre :");
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input).expect("Erreur de lecture");
+                let num1: f64 = input.trim().parse().expect("Veuillez entrer un nombre valide");
+
+                println!("Entrez le deuxième nombre :");
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input).expect("Erreur de lecture");
+                let num2: f64 = input.trim().parse().expect("Veuillez entrer un nombre valide");
+
+                let result = num1 + num2;
+                println!("Le résultat de l'addition est : {}", result);
+            },
+            2 => 
+            {
+                println!("Entrez le premier nombre :");
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input).expect("Erreur de lecture");
+                let num1: f64 = input.trim().parse().expect("Veuillez entrer un nombre valide");
+
+                println!("Entrez le deuxième nombre :");
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input).expect("Erreur de lecture");
+                let num2: f64 = input.trim().parse().expect("Veuillez entrer un nombre valide");
+
+                let result = num1 - num2;
+                println!("Le résultat de la soustraction est : {}", result);
+            },
+            3 => 
+            {
+                println!("Entrez le premier nombre :");
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input).expect("Erreur de lecture");
+                let num1: f64 = input.trim().parse().expect("Veuillez entrer un nombre valide");
+
+                println!("Entrez le deuxième nombre :");
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input).expect("Erreur de lecture");
+                let num2: f64 = input.trim().parse().expect("Veuillez entrer un nombre valide");
+
+                let result = num1 * num2;
+                println!("Le résultat de la multiplication est : {}", result);
+            },
+            4 => 
+            {
+                println!("Entrez le premier nombre :");
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input).expect("Erreur de lecture");
+                let num1: f64 = input.trim().parse().expect("Veuillez entrer un nombre valide");
+
+                println!("Entrez le deuxième nombre :");
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input).expect("Erreur de lecture");
+                let num2: f64 = input.trim().parse().expect("Veuillez entrer un nombre valide");
+
+                if num2 == 0.0 {
+                    println!("Erreur : Division par zéro n'est pas autorisée.");
+                } else {
+                    let result = num1 / num2;
+                    println!("Le résultat de la division est : {}", result);
+                }
+            },
+            0 => println!("Merci d'avoir utilisé notre calculatrice personnelle. Au revoir !"),
+            _ => println!("Option invalide, veuillez réessayer."),
+        }
+    }
+
+
 }
 
 fn ex1() {
